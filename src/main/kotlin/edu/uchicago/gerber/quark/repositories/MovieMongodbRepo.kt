cad89a8f0fe70daf46e8db07d9cdc1615f3a2451
Adam Gerber <adam.gerber@yahoo.com>
2023-07-10 15:51:49 -0500

    use guide

--- src/main/kotlin/edu/uchicago/gerber/quark/repositories/MovieMongodbRepo.kt
+++ src/main/kotlin/edu/uchicago/gerber/quark/repositories/MovieMongodbRepo.kt
@@ -20,6 +20,9 @@ import java.util.stream.Stream
 @ApplicationScoped
 class MovieMongodbRepo: PanacheMongoRepository<Movie> {
 
+    //use this guide to create various CRUD ops
+    //https://quarkus.io/guides/mongodb-panache-kotlin
+
 //
 //    @Inject
 //    lateinit var mongoClient: MongoClient
