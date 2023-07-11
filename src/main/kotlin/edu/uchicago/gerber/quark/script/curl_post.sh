a22441ab4c9df0b391b9a52ed53f0fef8c86abf3
Adam Gerber <adam.gerber@yahoo.com>
2023-07-11 13:30:58 -0500

    curl command working

--- src/main/kotlin/edu/uchicago/gerber/quark/script/curl_post.sh
+++ src/main/kotlin/edu/uchicago/gerber/quark/script/curl_post.sh
@@ -1,5 +1,5 @@
-curl -X PUT localhost:8080/beers/
-   -H 'Content-Type: application/json'
+curl -X PUT localhost:8080/beers/ \
+   -H 'Content-Type: application/json' \
    -d '{"id":"64ad9d0136a6f71d17d36453", "name":"TestName", "hop":"TestHop", "malt":"TestMalt", "style":"TestStyle",
    "yeast":"TestYeast"}'
 
