3979601f91a9afdce301be9d5cbe14b1932a5cb4
Adam Gerber <adam.gerber@yahoo.com>
2023-07-11 22:08:37 -0500

    clena up9

--- src/main/kotlin/edu/uchicago/gerber/quark/repositories/MongoBeerRepository.kt
+++ src/main/kotlin/edu/uchicago/gerber/quark/repositories/MongoBeerRepository.kt
@@ -24,11 +24,6 @@ class MongoBeerRepository: PanacheMongoRepository<Beer>, BeerRepoInterface {
 
     }
 
-    //use this guide to create various CRUD ops
-    //https://quarkus.io/guides/mongodb-panache-kotlin
-
-    //guide to paging
-    //https://quarkus.io/guides/hibernate-orm-panache#paging
 
     //todo remove unnessary dependencies
     //fix maven home and surefire.
@@ -40,7 +35,7 @@ class MongoBeerRepository: PanacheMongoRepository<Beer>, BeerRepoInterface {
     override fun _create(beer: Beer){
         this.persist(beer)
     }
-    //create(List<Beer>)
+
 
     override fun _create(beers: List<Beer>){
         this.persist(beers)
@@ -51,14 +46,13 @@ class MongoBeerRepository: PanacheMongoRepository<Beer>, BeerRepoInterface {
        return this.findById(beerId) ?: throw Exception("No beer with that ID")
     }
 
-    //stream all
+
     override fun _readAll(): List<Beer> {
         return  this.listAll()
         //we can also stream using this.streamAll()
     }
 
     //UPDATE
-    //update(updateBeer)
 
     override fun _update(updatedBeer: Beer) {
        this.update(updatedBeer);
@@ -66,7 +60,7 @@ class MongoBeerRepository: PanacheMongoRepository<Beer>, BeerRepoInterface {
     }
 
     //DELETE
-    //delete(id)
+
 
     override fun _deleteById(id:String){
       val beerId = ObjectId(id)
@@ -84,6 +78,7 @@ class MongoBeerRepository: PanacheMongoRepository<Beer>, BeerRepoInterface {
         return this.count()
     }
 
+    //this returns a lazy query object
     override fun _findAll(): PanacheQuery<Beer>? {
         return this.findAll()
     }
